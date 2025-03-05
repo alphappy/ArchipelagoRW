7ce7ce79e9f3a4cbe0a1b40627c3aeda7ed586f1
alphappy <alphappyalphappy@gmail.com>
2025-03-05 15:16:31 -0600

    cleanup GameStateFlag - precompute scug masks instead of calculating them every call - _prefix a few more things

--- worlds/rain_world/conditions/gamestate.py
+++ worlds/rain_world/conditions/gamestate.py
@@ -3,50 +3,47 @@ from typing import Iterable
 
 class GameStateFlag:
     """Encapsulates a set of gamestates with a bitflag."""
-
     value: int
+
     def __init__(self, vanilla: Iterable[str] | int | None = None,
                  msc: Iterable[str] | None = None, whitelist: bool = False):
         if type(vanilla) == int:
             self.value = vanilla
-        elif whitelist:
-            self.value = 0
-            if vanilla is not None:
-                self.set_scugs("Vanilla", vanilla, True)
-            if msc is not None:
-                self.set_scugs("MSC", msc, True)
         else:
-            self.value = 0b111_111_111_111
+            self.value = 0 if whitelist else 0b111_111_111_111
             if vanilla is not None:
-                self.set_scugs("Vanilla", vanilla, False)
+                self.set_scugs("Vanilla", vanilla, whitelist)
             if msc is not None:
-                self.set_scugs("MSC", msc, False)
+                self.set_scugs("MSC", msc, whitelist)
 
-    _index = {
-        "Vanilla": {s: i for i, s in enumerate(('Yellow', 'White', 'Red'))},
-        "MSC": {s: i+3 for i, s in enumerate(
+    _scug_masks = {
+        "Vanilla": {s: 1 << i for i, s in enumerate(('Yellow', 'White', 'Red'))},
+        "MSC": {s: 1 << (i+3) for i, s in enumerate(
             ('Yellow', 'White', 'Red', 'Gourmand', 'Artificer', 'Rivulet', 'Spear', 'Saint', 'Inv')
         )}
     }
 
-    _dlcstate_mask = {"Vanilla": 0b111, "MSC": 0b111_111_111_000}
+    _dlcstate_masks = {"Vanilla": 0b111, "MSC": 0b111_111_111_000}
 
     @staticmethod
-    def bit(dlcstate: str, scug: str): return GameStateFlag._index[dlcstate][scug]
+    def _get_mask(dlcstate: str, scug: str | None = None):
+        if scug is None:
+            return GameStateFlag._dlcstate_masks[dlcstate]
+        return GameStateFlag._scug_masks[dlcstate][scug]
 
     def set_scugs(self, dlcstate: str, scugs: Iterable[str], value: bool):
-        self.set_mask(sum(1 << GameStateFlag.bit(dlcstate, scug) for scug in set(scugs)), value)
+        self._set_mask(sum(self._get_mask(dlcstate, scug) for scug in set(scugs)), value)
 
     def set_dlcstate(self, dlcstate: str, value: bool):
-        self.set_mask(GameStateFlag._dlcstate_mask[dlcstate], value)
+        self._set_mask(self._get_mask(dlcstate), value)
 
-    def set_mask(self, mask: int, value: bool):
+    def _set_mask(self, mask: int, value: bool):
         if value:
             self.value |= mask
         else:
             self.value = ~(~self.value | mask)
 
-    def get(self, dlcstate: str, scug: str): return self.value & (1 << GameStateFlag.bit(dlcstate, scug)) != 0
+    def get(self, dlcstate: str, scug: str): return self.value & (self._get_mask(dlcstate, scug)) != 0
 
     def __getitem__(self, item: tuple[str, str]): return self.get(item[0], item[1])
 
