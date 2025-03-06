6fa823fc7058dadf5e42d7ac05ae677073564601
alphappy <alphappyalphappy@gmail.com>
2025-03-06 11:28:09 -0600

    rework echo logic a bit - MS echo added - generation and access conditions separated - access conditions set closer to the source

--- worlds/rain_world/locations/__init__.py
+++ worlds/rain_world/locations/__init__.py
@@ -6,7 +6,7 @@ from ..options import RainWorldOptions
 # Tokens/Pearls        0 -  999
 # Unique            4900 - 4999
 # Passages          5000 - 5046
-# Echoes            5070 - 5079
+# Echoes            5070 - 5080
 # Food Quest        5250 - 5271
 #   Expanded        5272 - 5305
 
@@ -16,6 +16,6 @@ def generate(options: RainWorldOptions) -> list[LocationData]:
         *tokens_pearls.generate(options),
         *unique.generate(options),
         *passages.generate(options),
-        *echoes.locations,
+        *echoes.generate(options),
         *foodquest.generate(options),
     ]
