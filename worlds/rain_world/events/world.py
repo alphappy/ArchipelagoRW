alphappy/ArchipelagoRW

commit 13b9bab3a5eca21c8f7b4efc091d6650b0613343

apply fix to static world events as well

worlds/rain_world/events/world.py
@@ -1,47 +1,45 @@
 from .classes import StaticWorldEvent
+from ..conditions import GameStateFlag
 from ..options import RainWorldOptions
 
-from ..game_data.general import scugs_all
+from ..game_data.general import scugs_all, scugs_vanilla
 from ..game_data import static_data
 from ..regions.classes import RainWorldRegion, room_to_region
-from ..utils import effective_blacklist
+from ..utils import (placed_object_effective_whitelist as POEW, creature_den_effective_whitelist as CDEW,
+                     room_effective_whitelist as REW)
 
 
 def generate_events_for_one_gamestate(options: RainWorldOptions,
                                       regions: list[RainWorldRegion]) -> list[StaticWorldEvent]:
     ret = []
+    scugs = scugs_all if options.dlcstate == "MSC" else scugs_vanilla
 
     for region in regions:
         if (rooms := region.rooms) and len(rooms) > 0:
             if region_data := static_data[options.dlcstate].get(region.code[:2], {}):
-                events = {}
+                events: dict[str, GameStateFlag] = {}
 
                 for room in rooms:
                     if room_data := region_data.get(room, {}):
                         for obj_type, obj_data in room_data.get("objects", {}).items():
-                            if len(current := events.get(obj_type, set(scugs_all))) > 0:
-                                incoming = effective_blacklist(
-                                    obj_data.get("filter", None), obj_data.get("whitelist", None), room_data
-                                )
-                                events[obj_type] = current.intersection(incoming)
+                            flag = events.setdefault(obj_type, GameStateFlag(0))
+                            flag[options.dlcstate, POEW(room_data, obj_data, scugs)] = True
 
                         for crit_type, crit_data in room_data.get("spawners", {}).get("normal", {}).items():
-                            if len(current := events.get(crit_type, set(scugs_all))) > 0:
-                                events[crit_type] = current.intersection(set(scugs_all).difference(crit_data))
+                            flag = events.setdefault(crit_type, GameStateFlag(0))
+                            flag[options.dlcstate, CDEW(room_data, crit_data, scugs)] = True
 
                         if room_tags := room_data.get("tags", []):
-                            if "SWARMROOM" in room_tags:
-                                events["Fly"] = set()
-                            if "SCAVOUTPOST" in room_tags:
-                                events["Toll"] = set()
-                            if "SHELTER" in room_tags:
-                                events["Shelter"] = set()
+                            for tagname, eventname in (
+                                    ("SWARMROOM", "Fly"), ("SCAVOUTPOST", "Toll"), ("SHELTER", "Shelter")
+                            ):
+                                if tagname in room_tags:
+                                    flag = events.setdefault(eventname, GameStateFlag(0))
+                                    flag[options.dlcstate, REW(room_data, scugs)] = True
 
-                for event_type, event_blacklist in events.items():
-                    ret.append(StaticWorldEvent(
-                        event_type, f'{region.name} {event_type}', region.name,
-                        scugs=set(scugs_all).difference(event_blacklist)
-                    ))
+                for event_type, event_flag in events.items():
+                    if options.satisfies(event_flag):
+                        ret.append(StaticWorldEvent(event_type, f'{region.name} {event_type}', region.name))
 
     # HARDCODE
     for room in ("SS_E07", "SL_AI", "RM_AI", "DM_AI", "LC_LAB01"):

worlds/rain_world/utils.py
@@ -1,7 +1,5 @@
 from typing import TypeVar
 
-from .game_data.general import scugs_all
-
 T = TypeVar("T")
 
 
@@ -47,16 +45,31 @@ def flounder2(weights: dict[T, float], count: int) -> list[T]:
     return ret[:count]
 
 
-def effective_blacklist(po_blacklist: set[str] | None, po_whitelist: set[str] | None, room_data: dict):
-    room_blacklist = room_data.get("blacklist", set(scugs_all).difference(room_data.get("whitelist", set(scugs_all))))
-    if alted := room_data.get("alted", None):
-        if po_blacklist is None:  # exclusively in alt settings files
-            ret = set(scugs_all).difference(po_whitelist or set()).union(room_blacklist)
-        else:  # in both alt settings files and base settings file
-            ret = po_blacklist.union(alted).difference(po_whitelist or set()).union(room_blacklist)
-    else:  # no alt settings files for this room
-        ret = (po_blacklist or set()).union(room_blacklist)
+def placed_object_effective_whitelist(room_data: dict, shiny_data: dict, scuglist: set[str]):
+    return (
+        room_data.get("whitelist", set(scuglist))
+        .difference(room_data.get("blacklist", set()))
+        .difference(shiny_data.get("filter", set()))
+        .difference(room_data.get("alted", set()))
+        .union(shiny_data.get("whitelist", set()))
+        .difference({""})
+    )
 
-    return ret.difference({""})
+
+def creature_den_effective_whitelist(room_data: dict, den_data: set[str], scuglist: set[str]):
+    return (
+        room_data.get("whitelist", set(scuglist))
+        .difference(room_data.get("blacklist", set()))
+        .intersection(den_data)
+    )
+
+
+def room_effective_whitelist(room_data: dict, scuglist: set[str]):
+    return (
+        room_data.get("whitelist", set(scuglist))
+        .difference(room_data.get("blacklist", set()))
+        # .difference(room_data.get("alted", set()))  TODO this does matter but it can't be handled like this
+        .difference({""})
+    )
 
 

commit ff6d3b6d53641a973fd71b5cafc77cf9fd2f69e6

fix effective whitelist regression caused by 13b9bab - this fix also revealed that sofanthiel needs submerged to get glow weed (combined into this commit because otherwise a unit test fails)

worlds/rain_world/events/world.py
@@ -24,11 +24,11 @@ def generate_events_for_one_gamestate(options: RainWorldOptions,
                     if room_data := region_data.get(room, {}):
                         for obj_type, obj_data in room_data.get("objects", {}).items():
                             flag = events.setdefault(obj_type, GameStateFlag(0))
-                            flag[options.dlcstate, POEW(room_data, obj_data, scugs)] = True
+                            flag[options.dlcstate, POEW(room_data, obj_data, set(scugs))] = True
 
                         for crit_type, crit_data in room_data.get("spawners", {}).get("normal", {}).items():
                             flag = events.setdefault(crit_type, GameStateFlag(0))
-                            flag[options.dlcstate, CDEW(room_data, crit_data, scugs)] = True
+                            flag[options.dlcstate, CDEW(room_data, crit_data, set(scugs))] = True
 
                         if room_tags := room_data.get("tags", []):
                             for tagname, eventname in (
@@ -36,7 +36,7 @@ def generate_events_for_one_gamestate(options: RainWorldOptions,
                             ):
                                 if tagname in room_tags:
                                     flag = events.setdefault(eventname, GameStateFlag(0))
-                                    flag[options.dlcstate, REW(room_data, scugs)] = True
+                                    flag[options.dlcstate, REW(room_data, set(scugs))] = True
 
                 for event_type, event_flag in events.items():
                     if options.satisfies(event_flag):

worlds/rain_world/locations/foodquest.py
@@ -39,6 +39,9 @@ class FoodQuestPip(AbstractLocation):
             return False
         if self.expanded and not options.checks_foodquest_expanded:
             return False
+        # HARDCODE: Sofanthiel's only glow weed is in MS.  The interactive map is wrong.
+        if options.starting_scug == "Inv" and options.checks_submerged < 2 and self.items[0] == "GlowWeed":
+            return False
         return super().pre_generate(player, multiworld, options)
 
 

worlds/rain_world/utils.py
@@ -46,12 +46,14 @@ def flounder2(weights: dict[T, float], count: int) -> list[T]:
 
 
 def placed_object_effective_whitelist(room_data: dict, shiny_data: dict, scuglist: set[str]) -> set[str]:
+    maybe_whitelist = shiny_data.get("whitelist", set())
+
     return (
         room_data.get("whitelist", set(scuglist))
         .difference(room_data.get("blacklist", set()))
-        .difference(shiny_data.get("filter", set()))
+        .difference(shiny_data.get("filter", scuglist.difference(maybe_whitelist)))
         .difference(room_data.get("alted", set()))
-        .union(shiny_data.get("whitelist", set()))
+        .union(maybe_whitelist)
         .difference({""})
     )
 