5e63c0cd8cbeeb4483ba7de1f1fcc5d3ca492acc
alphappy <alphappyalphappy@gmail.com>
2025-06-09 18:03:39 -0500

    update foodquest to not use GameStateFlag

--- worlds/rain_world/locations/foodquest.py
+++ worlds/rain_world/locations/foodquest.py
@@ -1,8 +1,8 @@
 from BaseClasses import MultiWorld
 from .classes import LocationData, AbstractLocation
-from ..conditions import GameStateFlag
 from ..conditions.classes import Simple
 from ..game_data.general import extreme_threat_creatures, wiki_names
+from ..game_data.bitflag import ScugFlag
 from ..options import RainWorldOptions
 
 _offset = 5250
@@ -17,7 +17,7 @@ class FoodQuestPip(AbstractLocation):
             names.insert(0, f"Food Quest - {proper_name}")
         super().__init__(names[0], names[-1], names[1:-1], _offset, "Food Quest")
         _offset += 1
-        self.gamestates = GameStateFlag(gamestates)
+        self.scugflag = ScugFlag(gamestates)
         self.expanded = False
         self.access_condition = Simple(items, 1)
 
@@ -33,7 +33,7 @@ class FoodQuestPip(AbstractLocation):
         return ret
 
     def pre_generate(self, player: int, multiworld: MultiWorld, options: RainWorldOptions) -> bool:
-        if not options.satisfies(self.gamestates):
+        if options.starting_scug not in self.scugflag:
             return False
         if self.items[0] in extreme_threat_creatures and not options.difficulty_extreme_threats:
             return False
@@ -46,60 +46,60 @@ class FoodQuestPip(AbstractLocation):
 
 
 pips: list[FoodQuestPip] = [
-    FoodQuestPip(0b110_111_111_000, "SlimeMold"),
-    FoodQuestPip(0b110_111_111_000, "DangleFruit"),
-    FoodQuestPip(0b101_111_111_000, "Fly"),
-    FoodQuestPip(0b111_111_111_000, "Mushroom"),
-    FoodQuestPip(0b101_011_100_000, "BlackLizard"),
-    FoodQuestPip(0b110_111_111_000, "WaterNut"),
-    FoodQuestPip(0b100_111_111_000, "JellyFish"),
-    FoodQuestPip(0b101_011_100_000, "JetFish"),
+    FoodQuestPip(0b1_110_111_111, "SlimeMold"),
+    FoodQuestPip(0b1_110_111_111, "DangleFruit"),
+    FoodQuestPip(0b1_101_111_111, "Fly"),
+    FoodQuestPip(0b1_111_111_111, "Mushroom"),
+    FoodQuestPip(0b0_101_011_100, "BlackLizard"),
+    FoodQuestPip(0b1_110_111_111, "WaterNut"),
+    FoodQuestPip(0b1_100_111_111, "JellyFish"),
+    FoodQuestPip(0b0_101_011_100, "JetFish"),
     # Artificer can eat glow weed, but there are none in that gamestate.
-    FoodQuestPip(0b110_101_111_000, "GlowWeed"),
-    FoodQuestPip(0b101_011_100_000, ["Salamander", "EelLizard"]),
-    FoodQuestPip(0b101_011_100_000, "Snail"),
-    FoodQuestPip(0b101_111_111_000, ["Hazer", "DeadHazer"]),
-    FoodQuestPip(0b101_011_100_000, "EggBug"),
-    FoodQuestPip(0b110_111_111_000, "LillyPuck"),
-    FoodQuestPip(0b101_011_100_000, "YellowLizard"),
-    FoodQuestPip(0b101_011_100_000, "TubeWorm"),
-    FoodQuestPip(0b111_111_111_000, "SSOracleSwarmer"),
-    FoodQuestPip(0b101_111_111_000, "Centiwing"),
-    FoodQuestPip(0b110_111_111_000, "DandelionPeach"),
-    FoodQuestPip(0b101_011_100_000, "CyanLizard"),
-    FoodQuestPip(0b110_111_111_000, "GooieDuck"),
-    FoodQuestPip(0b101_111_111_000, ["RedCentipede", "AquaCenti"]),
+    FoodQuestPip(0b1_110_101_111, "GlowWeed"),
+    FoodQuestPip(0b0_101_011_100, ["Salamander", "EelLizard"]),
+    FoodQuestPip(0b0_101_011_100, "Snail"),
+    FoodQuestPip(0b1_101_111_111, ["Hazer", "DeadHazer"]),
+    FoodQuestPip(0b0_101_011_100, "EggBug"),
+    FoodQuestPip(0b1_110_111_111, "LillyPuck"),
+    FoodQuestPip(0b0_101_011_100, "YellowLizard"),
+    FoodQuestPip(0b0_101_011_100, "TubeWorm"),
+    FoodQuestPip(0b0_111_111_111, "SSOracleSwarmer"),
+    FoodQuestPip(0b0_101_111_111, "Centiwing"),
+    FoodQuestPip(0b1_110_111_111, "DandelionPeach"),
+    FoodQuestPip(0b0_101_011_100, "CyanLizard"),
+    FoodQuestPip(0b1_110_111_111, "GooieDuck"),
+    FoodQuestPip(0b1_101_111_111, ["RedCentipede", "AquaCenti"]),
 
-    FoodQuestPip(0b111_111_111_000, "SeedCob"),
-    FoodQuestPip(0b101_111_111_000, ["Centipede", "SmallCentipede"], "Centipede"),
-    FoodQuestPip(0b101_111_111_000, ["VultureGrub", "DeadVultureGrub"]),
-    FoodQuestPip(0b101_111_111_000, ["SmallNeedleWorm", "BigNeedleWorm"], "Noodlefly"),
-    FoodQuestPip(0b101_011_100_000, "GreenLizard"),
-    FoodQuestPip(0b101_011_100_000, "BlueLizard"),
-    FoodQuestPip(0b101_011_100_000, "PinkLizard"),
-    FoodQuestPip(0b101_011_100_000, "WhiteLizard"),
-    FoodQuestPip(0b101_011_100_000, "RedLizard"),
-    FoodQuestPip(0b101_011_100_000, "SpitLizard"),
-    FoodQuestPip(0b100_000_000_000, "ZoopLizard"),
-    FoodQuestPip(0b100_000_000_000, "TrainLizard"),
-    FoodQuestPip(0b101_011_100_000, "BigSpider"),
-    FoodQuestPip(0b101_011_100_000, "SpitterSpider"),
-    FoodQuestPip(0b101_011_100_000, "MotherSpider"),
+    FoodQuestPip(0b1_111_111_111, "SeedCob"),
+    FoodQuestPip(0b1_101_111_111, ["Centipede", "SmallCentipede"], "Centipede"),
+    FoodQuestPip(0b1_101_111_111, ["VultureGrub", "DeadVultureGrub"]),
+    FoodQuestPip(0b1_101_111_111, ["SmallNeedleWorm", "BigNeedleWorm"], "Noodlefly"),
+    FoodQuestPip(0b0_101_011_100, "GreenLizard"),
+    FoodQuestPip(0b0_101_011_100, "BlueLizard"),
+    FoodQuestPip(0b0_101_011_100, "PinkLizard"),
+    FoodQuestPip(0b0_101_011_100, "WhiteLizard"),
+    FoodQuestPip(0b0_101_011_100, "RedLizard"),
+    FoodQuestPip(0b0_101_011_100, "SpitLizard"),
+    FoodQuestPip(0b0_100_000_000, "ZoopLizard"),
+    FoodQuestPip(0b0_100_000_000, "TrainLizard"),
+    FoodQuestPip(0b0_101_011_100, "BigSpider"),
+    FoodQuestPip(0b0_101_011_100, "SpitterSpider"),
+    FoodQuestPip(0b0_101_011_100, "MotherSpider"),
     # All vultures are Miros for Sofanthiel.  There are no Miros for Hunter/Gourmand.
-    FoodQuestPip(0b001_011_100_000, "Vulture"),
-    FoodQuestPip(0b001_011_100_000, "KingVulture"),
-    FoodQuestPip(0b101_010_000_000, "MirosVulture"),
-    FoodQuestPip(0b101_011_100_000, "LanternMouse"),
-    FoodQuestPip(0b101_011_100_000, ["CicadaA", "CicadaB"], "Squidcada"),
-    FoodQuestPip(0b100_001_000_000, "Yeek"),
-    FoodQuestPip(0b101_011_100_000, "DropBug"),
-    FoodQuestPip(0b101_011_100_000, "MirosBird"),
-    FoodQuestPip(0b101_011_100_000, ["Scavenger", "ScavengerElite"], "Scavenger"),
-    FoodQuestPip(0b101_011_100_000, ["DaddyLongLegs", "BrotherLongLegs", "TerrorLongLegs", "HunterDaddy"], "Rot"),
-    FoodQuestPip(0b001_000_000_000, "PoleMimic"),
-    FoodQuestPip(0b001_000_000_000, "TentaclePlant"),
-    FoodQuestPip(0b001_000_000_000, "BigEel"),
-    FoodQuestPip(0b001_000_000_000, "Inspector"),
+    FoodQuestPip(0b0_001_011_100, "Vulture"),
+    FoodQuestPip(0b0_001_011_100, "KingVulture"),
+    FoodQuestPip(0b0_101_010_000, "MirosVulture"),
+    FoodQuestPip(0b0_101_011_100, "LanternMouse"),
+    FoodQuestPip(0b0_101_011_100, ["CicadaA", "CicadaB"], "Squidcada"),
+    FoodQuestPip(0b0_100_001_000, "Yeek"),
+    FoodQuestPip(0b0_101_011_100, "DropBug"),
+    FoodQuestPip(0b0_101_011_100, "MirosBird"),
+    FoodQuestPip(0b0_101_011_100, ["Scavenger", "ScavengerElite"], "Scavenger"),
+    FoodQuestPip(0b0_101_011_100, ["DaddyLongLegs", "BrotherLongLegs", "TerrorLongLegs", "HunterDaddy"], "Rot"),
+    FoodQuestPip(0b0_001_000_000, "PoleMimic"),
+    FoodQuestPip(0b0_001_000_000, "TentaclePlant"),
+    FoodQuestPip(0b0_001_000_000, "BigEel"),
+    FoodQuestPip(0b0_001_000_000, "Inspector"),
 ]
 
 for i in range(22, len(pips)):
