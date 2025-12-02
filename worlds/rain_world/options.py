2840adfdbb511fb308bc6f07504fb8bb2f0673ec
SaltySyrup <sltysyrp@gmail.com>
2025-12-01 18:28:47 -0600

    Fix options to have every option exist inside a defined group

--- worlds/rain_world/options.py
+++ worlds/rain_world/options.py
@@ -839,8 +839,8 @@ class RainWorldOptions(PerGameCommonOptions, DeathLinkMixin):
     debug_output: DebugOutput
 
     group_important = [
-        WhichGameVersion, IsMSCEnabled, IsWatcherEnabled, WhichCampaign,
-        PassageProgressWithoutSurvivor, WhichVictoryCondition, WhichGateBehavior, DeathLink, RandomStartingRegion
+        WhichGameVersion, IsMSCEnabled, IsWatcherEnabled, WhichCampaign, PassageProgressWithoutSurvivor,
+        WhichVictoryCondition, WhichGateBehavior, DeathLink, RandomStartingRegion, DebugOutput
     ]
 
     #################################################################
@@ -903,9 +903,9 @@ class RainWorldOptions(PerGameCommonOptions, DeathLinkMixin):
     so_peeping: SoPeeping
 
     group_watcher = [
-        LogicRottedGeneration, LogicMinRippleTarget, NormalDynamicWarpBehavior, ThroneDynamicWarpBehavior,
+        SoPeeping, LogicRottedGeneration, LogicMinRippleTarget, NormalDynamicWarpBehavior, ThroneDynamicWarpBehavior,
         DynamicWarpPoolSize, RottedRegionTarget, ChecksSpreadRot, ChecksSpreadRotProgressive,
-        SpinningTopKeys, PriorityThrone,
+        SpinningTopKeys, PriorityThrone, PredeterminedDynamicWarpNetworkMinimumNecklaceLength,
     ]
 
     #################################################################
@@ -1091,7 +1091,7 @@ option_groups = [
     OptionGroup("Difficulty settings", RainWorldOptions.group_difficulty, True),
     OptionGroup("Check pool settings", RainWorldOptions.group_checkpool, True),
     OptionGroup("Item pool settings", RainWorldOptions.group_itempool, True),
-    # OptionGroup("Watcher-specific settings (spoilers)", RainWorldOptions.group_watcher, True),
+    OptionGroup("Watcher-specific settings", RainWorldOptions.group_watcher, True),
     OptionGroup("Filler item relative weights", RainWorldOptions.group_filler, True),
     OptionGroup("Trap relative weights", RainWorldOptions.group_traps, True),
 ]
