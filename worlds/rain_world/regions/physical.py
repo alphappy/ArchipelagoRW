6cbacbc8811c841c7c1aadf52abda3b35fd94d02
SaltySyrup <sltysyrp@gmail.com>
2025-12-09 23:31:29 -0600

    Modify Watcher physical regions
    - Coral Caves split removed, the gap in the pipes was made short enough to feasibly jump across
    - Badlands split removed, the silo entracnce was simplified and no longer contains anything interesting
    - Shattered Terrace conditions changed, the jump is doable with only ripple 5 and backwards is free
    - Migration Path split added, crossing that gap leftwards requires ripple 9

--- worlds/rain_world/regions/physical.py
+++ worlds/rain_world/regions/physical.py
@@ -157,17 +157,6 @@ def _generate(options: RainWorldOptions) -> list[PhysicalRegion | ConnectionData
                 if options.starting_scug == "Saint":
                     ret += [ConnectionData("Shoreline", "Shoreline above puppet", "Exit top of puppet room")]
 
-            case "WRFA":
-                western = {"WRFA_S08", "WRFA_C11", "WRFA_A21"}
-                remainder = rooms.difference(western)
-
-                ret += [
-                    PhysicalRegion("Coral Caves", "WRFA", remainder),
-                    PhysicalRegion("Coral Caves far west", "WRFA^1", western),
-                    ConnectionData("Coral Caves", "Coral Caves far west", "WRFA_E04 to WRFA_C11", Simple("Ripple", 8)),
-                    ConnectionData("Coral Caves far west", "Coral Caves", "WRFA_C11 to WRFA_E04", Simple("Ripple", 8)),
-                ]
-
             case "WARA":
                 western = {f"WARA_{r}" for r in {"E08", "P04", "P03", "P08", "P05", "P02", "S23", "P19"}}
                 remainder = rooms.difference(western)
@@ -175,18 +164,25 @@ def _generate(options: RainWorldOptions) -> list[PhysicalRegion | ConnectionData
                 ret += [
                     PhysicalRegion("Shattered Terrace", "WARA", remainder),
                     PhysicalRegion("Western Shattered Terrace", "WARA^1", western),
-                    ConnectionData("Shattered Terrace", "Western Shattered Terrace", "WARA_P20 to WARA_P19", Simple("Ripple", 8)),
-                    ConnectionData("Western Shattered Terrace", "Shattered Terrace", "WARA_P20 to WARA_P01", Simple("Ripple", 8)),
+                    # Rightwards through WARA_P20 just needs the minimum amount of float
+                    ConnectionData("Western Shattered Terrace", "Shattered Terrace", "WARA_P20 to WARA_P01",
+                                   Simple("Ripple", 4)),
+                    # Leftwards can be jumped easily
+                    ConnectionData("Shattered Terrace", "Western Shattered Terrace", "WARA_P20 to WARA_P19"),
                 ]
 
-            case "WBLA":
-                silo = {"WBLA_E02"}
-                remainder = rooms.difference(silo)
+            case "WMPA":
+                western = {f"WMPA{r}" for r in {"A09", "A06", "A04", "A02", "A05", "S01", "A03", "A01", "A07", "A08"}}
+                eastern = rooms.difference(western)
 
                 ret += [
-                    PhysicalRegion("Badlands", "WBLA", remainder),
-                    PhysicalRegion("Badlands inside silo", "WBLA^1", silo),
-                    ConnectionData("Badlands", "Badlands inside silo", "WBLA_F03 to WBLA_E02", Simple("Ripple", 8)),
+                    PhysicalRegion("Migration Path", "WMPA", eastern),
+                    PhysicalRegion("Western Migration Path", "WMPA^1", western),
+                    # Leftwards through the top of WMPA_B01 needs maximum float ability to cross
+                    ConnectionData("Migration Path", "Western Migration Path", "WMPA_B01 to WMPA_A08",
+                                   Simple("Ripple", 8)),
+                    # Rightwards is just a drop-down
+                    ConnectionData("Western Migration Path", "Migration Path", "WMPA_A08 to WMPA_B01"),
                 ]
 
             case _:
