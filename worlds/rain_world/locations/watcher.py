a2ccf151a0785571682347a7ec4ef44e0e94bfa0
SaltySyrup <sltysyrp@gmail.com>
2026-01-22 16:23:22 -0600

    Remove Throne warp locations
    - These are logically identical to Prince Encounters, making them redundant
    - Sometimes it can be strategic to not open all the throne warps you can ASAP, in which case having potential progression behind them can be undesirable

--- worlds/rain_world/locations/watcher.py
+++ worlds/rain_world/locations/watcher.py
@@ -35,36 +35,35 @@ class PrinceEncounter(RoomLocation):
         return super().make(player, multiworld, options)
 
 
-class ThroneWarp(RoomLocation):
-    def __init__(self, offset: int, num: int):
-        super().__init__(f"Create {self.names[num]} warp", f"ThroneWarp-{self.rooms[num]}", ["Throne Warp"], offset,
-                         f"WORA_THRONE{self.rooms[num]}")
+# class ThroneWarp(RoomLocation):
+#     def __init__(self, offset: int, num: int):
+#         super().__init__(f"Create {self.names[num]} warp", f"ThroneWarp-{self.rooms[num]}", ["Throne Warp"], offset,
+#                          f"WORA_THRONE{self.rooms[num]}")
+#
+#     rooms = [f"{a:0>2}" for a in (10, 5, 7, 9)]
+#     names = ["lower east", "lower west", "upper east", "upper west"]
+#
+#     def make(self, player: int, multiworld: MultiWorld, options: RainWorldOptions) -> bool:
+#         self.progress_type = options.priority_throne.value
+#         return super().make(player, multiworld, options)
 
-    rooms = [f"{a:0>2}" for a in (10, 5, 7, 9)]
-    names = ["lower east", "lower west", "upper east", "upper west"]
 
-    def make(self, player: int, multiworld: MultiWorld, options: RainWorldOptions) -> bool:
-        self.progress_type = options.priority_throne.value
-        return super().make(player, multiworld, options)
-
-
-def initialize() -> tuple[list[FixedWarpPoint], list[SpinningTop], list[Rottening], list[PrinceEncounter], list[ThroneWarp], list[LocationData]]:
+def initialize() -> tuple[list[FixedWarpPoint], list[SpinningTop], list[Rottening], list[PrinceEncounter], list[LocationData]]:
     return ([FixedWarpPoint(data, INITIAL_OFFSET + i) for i, data in enumerate(portals)],
             [SpinningTop(data, INITIAL_OFFSET + 100 + i) for i, data in enumerate(portals) if data.check_spinning_top],
             [Rottening(i + 1, INITIAL_OFFSET + 150 + i) for i in range(len(normal_regions))],
             [PrinceEncounter(INITIAL_OFFSET + 120 + i, i + 1) for i in range(4)],
-            [ThroneWarp(INITIAL_OFFSET + 125 + i, i) for i in range(4)],
             [RoomLocation("Meet Elder Ripple Spawn", "Meet_Ripple_Elder", [],
                           INITIAL_OFFSET + 130, "WORA_EGG")])
 
 
-fixed_warps, spinning_tops, rottenings, encounters, thrones, unique = initialize()
+fixed_warps, spinning_tops, rottenings, encounters, unique = initialize()
 
 
 def select(options: RainWorldOptions) -> list[LocationData]:
     if options.starting_scug != "Watcher":
         return []
-    ret = fixed_warps + spinning_tops + encounters + thrones + unique
+    ret = fixed_warps + spinning_tops + encounters + unique
     if options.should_have_rot_spread_checks:
         ret += rottenings
     return ret
